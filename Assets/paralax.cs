fa0fdde5fa04a1a736198f3520e3cb4bc6a1e5f4
KubaDobi <52657068+KubaDobi@users.noreply.github.com>
2019-07-10 15:54:26 +0300

    Wednesday End

--- Assets/paralax.cs
+++ Assets/paralax.cs
@@ -7,17 +7,23 @@ public class paralax : MonoBehaviour
     [SerializeField] Material Material;
     [SerializeField] float speed;
     [SerializeField] float yOffset;
+    [SerializeField] Rigidbody2D player;
+    float refCamSize;
+    float refScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        refCamSize = Camera.main.orthographicSize;
+        refScale = transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y + yOffset, transform.position.z);
-        Material.mainTextureOffset = new Vector2(Material.mainTextureOffset.x + speed * Time.deltaTime, 0);
+        Material.mainTextureOffset = new Vector2(Material.mainTextureOffset.x + speed * player.velocity.x * Time.deltaTime, 0);
+        float scale = refScale * (Camera.main.orthographicSize / refCamSize);
+        transform.localScale = new Vector3(scale, scale, scale);
     }
 }
