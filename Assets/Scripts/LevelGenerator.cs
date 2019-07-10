1ab872f09da6a6ca77d43c17fe5fecf10c5c4e16
Timo Sormunen <timsorm@gmail.com>
2019-07-10 11:20:43 +0300

    tweak obstacle generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    string EXIT_POINT_NAME = "exit";
     [SerializeField] GameObject[] tiles;
     [SerializeField] Transform firstTile;
     [SerializeField] int visibleTileAmount;
@@ -11,6 +12,9 @@ public class LevelGenerator : MonoBehaviour
     int tileCounter;
     List<Transform> visibleTiles;
     Camera playerCamera;
+
+    [SerializeField] GameObject[] obstacles;
+    [SerializeField] int maxObstaclesPerTile;
 
     void Awake()
     {
@@ -30,7 +34,7 @@ public class LevelGenerator : MonoBehaviour
     void Update()
     {
         // Add a new tile to the right when right side of first tile in the visibleTiles list exits screen
-        Vector3 firstVisiblePos = playerCamera.WorldToViewportPoint(visibleTiles[0].Find("exit").position);
+        Vector3 firstVisiblePos = playerCamera.WorldToViewportPoint(visibleTiles[0].Find(EXIT_POINT_NAME).position);
         bool isFirstVisibleOnScreen = firstVisiblePos.x > 0 && firstVisiblePos.x < 1;
         //Debug.Log(isFirstVisibleOnScreen);
         if (!isFirstVisibleOnScreen)
@@ -49,11 +53,11 @@ public class LevelGenerator : MonoBehaviour
 
     void AddNextTile()
     {
-        int nextTileIndex = Random.Range(0, tiles.Length);
-        GameObject nextTile = Instantiate(tiles[nextTileIndex]);
+        int typeIndex = Random.Range(0, tiles.Length);
+        GameObject nextTile = Instantiate(tiles[typeIndex]);
         visibleTiles.Add(nextTile.transform);
 
-        Transform exitPoint = currentTile.transform.Find("exit");
+        Transform exitPoint = currentTile.transform.Find(EXIT_POINT_NAME);
         nextTile.transform.position = new Vector2(exitPoint.position.x, exitPoint.position.y);
         
         if (tileCounter < visibleTileAmount)
@@ -65,7 +69,30 @@ public class LevelGenerator : MonoBehaviour
             Destroy(visibleTiles[0].gameObject);
             visibleTiles.RemoveAt(0);
         }
+
+        PopulateWithObstacles(nextTile);
         
         currentTile = nextTile;
+    }
+
+    void PopulateWithObstacles(GameObject tile)
+    {
+        int obstacleAmount = Random.Range(0, maxObstaclesPerTile + 1);
+        //Debug.Log(obstacleAmount);
+        for (int i = 0; i < obstacleAmount; i++)
+        {
+            AddObstacle(tile);
+        }
+    }
+
+    void AddObstacle(GameObject tile)
+    {
+        //Debug.Log(tile.transform.position + " " + tile.transform.Find(EXIT_POINT_NAME).position);
+        int typeIndex = Random.Range(0, obstacles.Length);
+        float fracJourney = 0.5f;
+        Vector3 pos = Vector3.Lerp(tile.transform.position, tile.transform.Find(EXIT_POINT_NAME).position, fracJourney);
+        //Debug.Log(pos);
+        GameObject obstacle = Instantiate(obstacles[typeIndex]);
+        obstacle.transform.position = pos;
     }
 }
